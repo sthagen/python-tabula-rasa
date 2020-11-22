a083ffe0d42bb2e3e4e72cde5c75ef67c451b110
Stefan Hagen <stefan@hagen.link>
2020-11-22 16:38:11 +0100

    Made test fail for stop token case

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -22,7 +22,7 @@ def test_main_ok_file(capsys):
     report_expected = (
         '3,1,42, * ,A KEY OR SO THEY SAY,A_KEY,A/N,4\n'
         '3,2,142, ,A LABEL OR WHATEVER,LABEL,A,42/84\n'
-        '3,3,2, ,NAME NAME (Version,8.1),A,123\n'
+        '3,3,2, ,NAME,NAME,A,123\n'
         '3,4,7, * ,ANOTHER KEY UNEXPECTEDLY,ANOTHER,N,3'
     )
     assert cli.main(job) is None
