2c9f9776070419dba6a0b82824ed4ae5216c976a
Stefan Hagen <stefan@hagen.link>
2020-11-22 15:48:51 +0100

    Added initial implementation - nothing to see here

---
+++ tabula_rasa/tabula_rasa.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+"""Add logical documentation here later TODO."""
+import sys
+ENCODING = 'utf-8'
+
+
+def parse(text):
+    """Temporary implementation to bootstrap testing."""
+    return text
