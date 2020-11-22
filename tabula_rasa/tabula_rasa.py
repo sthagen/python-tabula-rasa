f1d9cf7cd05dc3ae4dc97dff69d495fa932d0a5b
Stefan Hagen <stefan@hagen.link>
2020-11-22 16:03:02 +0100

    Added documentation of the target parser

--- tabula_rasa/tabula_rasa.py
+++ tabula_rasa/tabula_rasa.py
@@ -6,7 +6,24 @@ ENCODING = 'utf-8'
 
 
 def parse(text):
-    """Temporary implementation to bootstrap testing."""
+    """Temporary implementation to bootstrap testing.
+
+    Examples:
+    ---------
+    The inputs:
+        '3 .  1 .  42 * A KEY OR SO THEY SAY A_KEY A/N 4\n'
+        '3 .  2 .  142  A LABEL OR WHATEVER LABEL A 42/84\n'
+        '3 .  3 .  2  NAME NAME (Version 8.1) A 123\n'
+        '3 .  4 .  7 * ANOTHER KEY UNEXPECTEDLY ANOTHER N 3'
+
+    should yield:
+        3,1,42,*,A KEY OR SO THEY SAY,A_KEY,A/N,4
+        3,2,142, ,A LABEL OR WHATEVER LABEL,A,42/84
+        3,3,2, ,NAME,NAME,A,123
+        3,4,7,*,ANOTHER KEY UNEXPECTEDLY,ANOTHER,N,3
+    (one per call)
+    """
+
     return text
 
 
