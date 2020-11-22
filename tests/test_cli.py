8e4844181f89389387839475c23c279822d2fa77
Stefan Hagen <stefan@hagen.link>
2020-11-22 17:46:21 +0100

    Added cli test function to generate tsv test data file (now for real)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,9 @@ import tabula_rasa.cli as cli
 import tabula_rasa.tabula_rasa as tr
 
 
-TEXT_FIXTURE_PATH = pathlib.Path('tests', 'fixtures', 'text', 'tabula.txt')
+TEXT_FIXTURE_LEGEND_PATH = pathlib.Path('tests', 'fixtures', 'text', 'tabula.txt')
+CSV_FIXTURE_RULES_PATH = pathlib.Path('tests', 'fixtures', 'csv', 'tabula.txt')
+TSV_FIXTURE_DATA_PATH = pathlib.Path('tests', 'fixtures', 'tsv', 'tabula.txt')
 
 
 def test_main_ok_minimal(capsys):
@@ -20,7 +22,7 @@ def test_main_ok_minimal(capsys):
 
 
 def test_main_ok_file(capsys):
-    job = [str(TEXT_FIXTURE_PATH)]
+    job = [str(TEXT_FIXTURE_LEGEND_PATH)]
     report_expected = (
         '3,1,42,*,A KEY OR SO THEY SAY,A_KEY,A/N,4\n'
         '3,2,142, ,A LABEL OR WHATEVER,LABEL,A,42/84\n'
@@ -30,3 +32,19 @@ def test_main_ok_file(capsys):
     assert cli.main(job) is None
     out, err = capsys.readouterr()
     assert out.strip() == report_expected.strip()
+
+
+def generate_tsv_data():
+    tab = '\t'
+    out_path = str(TSV_FIXTURE_DATA_PATH)
+    data = (
+        ('A_KEY', 'LABEL', 'NAME', 'ANOTHER',),
+        ('EDDK', 'RUNWAY OR SO', 'IS IT JUST A NAME OR IS IT SOMETHING ELSE?', '123',),
+        ('KJFK', 'SOMETHING ACROSS THE OCEAN', 'DO NOT WORRY ABOUT THE NAME', '456',),
+    )
+    with open(out_path, 'wt', encoding=tr.ENCODING) as handle:
+        for row in data:
+            handle.write(tab.join(row))
+            handle.write('\n')
+
+    assert TSV_FIXTURE_DATA_PATH.is_file() is True
